6647c7d76334cdb648ecded57fda39dfda61547b
Kazuki Kishishita <kishi-tennis0310@rakuten.jp>
2018-09-19 14:43:00 +0800

    first commit

---
+++ Which one?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Which one?
+//
+//  Created by 岸下和樹 on 2018/09/19.
+//  Copyright © 2018年 岸下和樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Which one?Tests/Which_one_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Which_one_Tests.swift
+//  Which one?Tests
+//
+//  Created by 岸下和樹 on 2018/09/19.
+//  Copyright © 2018年 岸下和樹. All rights reserved.
+//
+
+import XCTest
+@testable import Which_one_
+
+class Which_one_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
